7051cca87a03c1579c55376a940dc85e3b3e9aa2
Jung U Kim <jukim20@cmc.edu>
2019-09-27 11:32:05 +0200

    Got ship to move and shoot projectiles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+
+    public float speed = 0.1f;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector3.left * speed);
+
+        }
+        if (Input.GetKey (KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector3.right * speed);
+        }
+
+        if (Input.GetKey (KeyCode.W))
+        {
+            gameObject.transform.Translate(Vector3.up * speed);
+        }
+
+        if (Input.GetKey (KeyCode.S))
+        {
+            gameObject.transform.Translate(Vector3.down * speed);
+        }
+
+        if (Input.GetKey (KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    public void Fire()
+    {
+        GameObject p = (GameObject) (Instantiate (projectile, transform.position + transform.up * 1.5f, Quaternion.identity));
+        p.GetComponent<Rigidbody2D>().AddForce(transform.up * 1000f);
+    }
+    
+}
