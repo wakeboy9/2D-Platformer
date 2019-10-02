5ea9341f4f218338f11b2ce457a1ce37cfe55330
Jung U Kim <jukim20@cmc.edu>
2019-10-02 15:25:59 +0200

    Made enemy move & bounce off walls in level 2

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class EnemyController : MonoBehaviour
 {
+    private Scene currentScene;
+    private string sceneName;
 
     public GameObject proj1; // Undestroyable projectile
     public GameObject proj2; // Destroyable projectile
@@ -16,27 +20,57 @@ public class EnemyController : MonoBehaviour
 
     private Transform target;
 
+    private float latestDirectionChangeTime;
+    private readonly float directionChangeTime = 3f;
+    private float velocity = 2f;
+    private Vector2 direction;
+    private Vector2 movementPerSecond;
+
     // Start is called before the first frame update
     void Start()
     {
         health = 5;
         invulnerable = false;
 
+        currentScene = SceneManager.GetActiveScene();
+
         target = GameObject.FindGameObjectWithTag("Player").transform;
         transform.up = (target.position - transform.position);
         StartCoroutine(shoot());
+
+        sceneName = currentScene.name;
+
+        if (sceneName == "Level2")
+        {
+            latestDirectionChangeTime = 0f;
+            calcuateNewMovementVector();
+        }
+
+        else if (sceneName == "Level3")
+        {
+
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if(health <= 0) {
             dead = true;
             Destroy(gameObject);
         }
 
         transform.up = (target.position - transform.position);
+
+        if (sceneName == "Level2")
+        {
+            transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
+            transform.position.y + (movementPerSecond.y * Time.deltaTime));
+        }
     }
+
 
     IEnumerator shoot() {
         while(!dead) {            
@@ -54,6 +88,23 @@ public class EnemyController : MonoBehaviour
                 health--;
             }
             Destroy(other.gameObject);
-        } 
+        }
+
+
+        // when the enemy hits the wall, bounce off of it
+        if (other.gameObject.tag == "Wall")
+        {
+            calcuateNewMovementVector();
+        }
     }
+
+    // make the enemy bounce off the wall by calculating new movement vector
+    void calcuateNewMovementVector()
+    {
+        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        direction = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        movementPerSecond = direction * velocity;
+    }
+
+
 }
