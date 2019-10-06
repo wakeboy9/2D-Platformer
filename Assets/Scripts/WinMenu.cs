366a019f4965aef2c2d4e813c0cd22cbb173849b
Madubuko Chiedozie <chiedoziemadubuko@gmail.com>
2019-10-06 14:12:03 +0200

    created win scene for after level 9

---
+++ Assets/Scripts/WinMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+    public GameObject myLevelHandler;
+    public void Start()
+    {
+        myLevelHandler = GameObject.FindGameObjectWithTag("LevelHandler");
+    }
+
+    public void RestartLevel()
+    {
+        //PlayerController.myActiveScene
+        SceneManager.LoadScene(LevelController.prevScene);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+}
